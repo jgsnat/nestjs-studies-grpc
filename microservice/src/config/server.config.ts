26a106fa97cf0c77994a45ac6baa69730b7fe724
Jefferson Silva <jgsnat@gmail.com>
2022-03-06 11:02:20 -0300

    - build: alteração do projeto para funcionar como server de comunicação grpc;

---
+++ microservice/src/config/server.config.ts
@@ -0,0 +1,11 @@
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+export const grpcServerOptions = {
+  transport: Transport.GRPC,
+  options: {
+    url: '127.0.0.1:6379',
+    protoPath: join(__dirname, '/hero/hero.proto'),
+    package: 'hero',
+  },
+};
